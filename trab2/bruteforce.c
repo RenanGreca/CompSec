374357a089fb38bb86f6f116eb8e65ad6863b5cb
Renan Greca <renangreca@gmail.com>
2016-09-26 17:38:45 -0300

    partially completed each part

---
+++ trab2/bruteforce.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h>
+
+#if defined(__APPLE__)
+#  define COMMON_DIGEST_FOR_OPENSSL
+#  include <CommonCrypto/CommonDigest.h>
+#  define SHA1 CC_SHA1
+#else
+#  include <openssl/md5.h>
+#endif
+
+static const unsigned char hash[17] = {0x8d, 0x7b, 0x35, 0x6e, 0xae, 0x43, 0xad, 0xcd, 0x6a, 0xd3, 0xee, 0x12, 0x4c, 0x3d, 0xcf, 0x1e, '\0'};
+static unsigned char digest[17];
+static const char alphabet[] =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"abcdefghijklmnopqrstuvwxyz"
+"0123456789";
+
+void compute_md5(char *str, unsigned char digest[16]) {
+    MD5_CTX ctx;
+    MD5_Init(&ctx);
+    MD5_Update(&ctx, str, strlen(str));
+    MD5_Final(digest, &ctx);
+}
+
+char *bruteForce(char *word, int index, int max_len) {
+
+    for (int i=11; i<62; i++) {
+        word[index] = alphabet[i];
+        word[index+1] = '\0';
+
+        compute_md5(word, digest);
+
+        if (!strcmp((char *) hash, (char *) digest)) {
+            return word;
+        }
+
+        if (index < max_len) {
+            char *res = bruteForce(word, index+1, max_len);
+            if (res != NULL) {
+                return res;
+            }
+        }
+        
+    }
+
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    char output[21];
+
+    char *word = malloc(9 * sizeof(char));
+
+    bruteForce(word, 0, 8);
+
+    printf("%s\n", word);
+
+    char input[20] = {0x0b, 0x34, 0x30, 0x20, 0x2f, 0x27, 0x05, 0x22, 0x05, 0x29, 0x21, 0x28, 0x22, 0x26, 0x19, 0x34, 0x23, 0x22, 0x27, 0x2d};
+
+    int len = strlen(word);
+
+    for (int i=0; i<20; i++) {
+        output[i] = input[i] ^ word[i % len];
+    }
+
+    output[20] = '\0';
+
+    printf("%s\n", output);
+
+    return 0;
+}
+
---
+++ trab2/wordlist.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+// #include <dirent.h>
+#define NAME_MAX 255
+#include "tinydir.h"
+
+void ReadWords(const char *filename, char *words[], int *count, int max_number_of_words) {
+    FILE *f = fopen(filename, "rt"); // checking for NULL is boring; i omit it
+    int i;
+    char temp[100]; // assuming the words cannot be too long
+
+    for (i = *count; i < max_number_of_words; ++i)
+    {
+        // Read a word from the file
+        if (fscanf(f, "%s", temp) != 1)
+            break;
+        // note: "!=1" checks for end-of-file; using feof for that is usually a bug
+
+        // Allocate memory for the word, because temp is too temporary
+        printf("%s\n", temp);
+        words[i] = strdup(temp);
+    }
+    fclose(f);
+
+    *count = i;
+}
+
+int main(int argc, char *argv[]) {
+
+    printf("Hello, world\n");
+    tinydir_dir dir;
+    int i;
+    tinydir_open_sorted(&dir, "/Users/renangreca/OneDrive/repos/compsec/trab2/legiaourbana");
+
+    char *words[2000];
+    int count = 0;
+
+    for (i = 0; i < dir.n_files; i++)
+    {
+        tinydir_file file;
+        tinydir_readfile_n(&dir, &file, i);
+
+        printf("%s\n", file.name);
+        
+        ReadWords(file.path, words, &count, 2000);
+
+    }
+
+    tinydir_close(&dir);
+    
+    printf("%d\n", count);
+
+    FILE *f = fopen("dict.txt", "w");
+    if (f == NULL)
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+
+    /* print some text */
+    for (int i = 0; i<count; i++) {
+        fprintf(f, "%s\n", words[i]);
+    }
+
+    fclose(f);
+    
+}
